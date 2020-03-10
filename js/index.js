9f121d2bdc0ccb6f4cf044ff5d730b96f64292d4
Christina Harris <christinathedesigner@outlook.com>
2020-03-10 09:02:33 -0700

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+
+let logo = document.querySelector("h1.logo-heading")
+
+logo.addEventListener("mouseover", event => 
+    event.target.style.color = "green");
+
+logo.addEventListener("mouseout", event => 
+    event.target.style.color = "red"
+);
+
+let navLinks = document.querySelectorAll("a.nav-link")
+
+navLinks.forEach(btn => {
+    btn.addEventListener("click", event => event.target.style.color = "blue" )
+})
+
+navLinks.forEach(btn => {
+    btn.addEventListener("dblclick", event => event.target.style.color = "darkSeaGreen" )
+})
+
+let mainImage = document.querySelector(".intro img")
+this.addEventListener("wheel", event => {
+
+    mainImage.setAttribute("src", "https://cdn.mos.cms.futurecdn.net/XWRZCcWsDjdG29nQYJuDpb.png")
+
+})
+
