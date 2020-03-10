a27f94c12a104209ba8cb0c5c00144f66d99b444
Christina Harris <christinathedesigner@outlook.com>
2020-03-10 16:43:36 -0700

    stretch

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 // Your code goes here
 
-import {gsap} from "gsap";
 
 let logo = document.querySelector("h1.logo-heading")
 
@@ -16,23 +15,40 @@ newLogo.textContent = "Swifty Bus"
 logo.addEventListener("mouseleave", event => event.target.replaceWith(newLogo)
 )
 
+gsap.to(".logo-heading",{duration: 3, x: 100});
+
 newLogo.addEventListener("mouseup", event => 
     event.target.style.color = "red"
 );
 
 let navLinks = document.querySelectorAll("a.nav-link")
 
+
 navLinks.forEach(btn => {
     btn.addEventListener("click", event => {
         event.stopPropagation()
         event.preventDefault()
         event.target.style.color = "blue" 
+
+    })
+})
+
+
+this.addEventListener("load", event => {
+    navLinks.forEach(btn => {
+    gsap.to(btn, {duration: 1, y: -20, ease: "bounce"})
+    setTimeout(() => {
+        gsap.to(btn, {duration: 1, y: 0, ease: "bounce"})
+    }, 2000);
     })
 })
 
 navLinks.forEach(btn => {
     btn.addEventListener("dblclick", event => event.target.style.color = "darkSeaGreen" )
 })
+
+
+
 
 let mainImage = document.querySelector(".intro img")
 this.addEventListener("wheel", event => {
@@ -48,6 +64,8 @@ bodyColor.addEventListener("keyup", event => event.target.style.backgroundColor 
 let shadowImg = document.querySelectorAll("img")
 shadowImg.forEach(element => element.addEventListener("mouseover", event => event.target.style.boxShadow = "4px 4px 8px #788DDE")
 )
+
+gsap.to("img", {duration: 5, borderRadius: "100%"});
 
 let colorPara = document.querySelectorAll("p")
 colorPara.forEach(element => element.addEventListener("mouseenter", event => event.target.style.color = "slateGray")
