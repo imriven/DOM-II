d9aef2f5710e27ae443ad0a38b0be490f55c7e0e
Christina Harris <christinathedesigner@outlook.com>
2020-03-10 15:04:47 -0700

    working on stretch

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Your code goes here
 
+import {gsap} from "gsap";
+
 let logo = document.querySelector("h1.logo-heading")
 
 logo.addEventListener("mouseover", event => 
@@ -9,10 +11,23 @@ logo.addEventListener("mouseout", event =>
     event.target.style.color = "red"
 );
 
+const newLogo = document.createElement("h1")
+newLogo.textContent = "Swifty Bus"
+logo.addEventListener("mouseleave", event => event.target.replaceWith(newLogo)
+)
+
+newLogo.addEventListener("mouseup", event => 
+    event.target.style.color = "red"
+);
+
 let navLinks = document.querySelectorAll("a.nav-link")
 
 navLinks.forEach(btn => {
-    btn.addEventListener("click", event => event.target.style.color = "blue" )
+    btn.addEventListener("click", event => {
+        event.stopPropagation()
+        event.preventDefault()
+        event.target.style.color = "blue" 
+    })
 })
 
 navLinks.forEach(btn => {
@@ -26,3 +41,17 @@ this.addEventListener("wheel", event => {
 
 })
 
+let bodyColor = document.querySelector("body")
+
+bodyColor.addEventListener("keyup", event => event.target.style.backgroundColor = "darkSalmon")
+
+let shadowImg = document.querySelectorAll("img")
+shadowImg.forEach(element => element.addEventListener("mouseover", event => event.target.style.boxShadow = "4px 4px 8px #788DDE")
+)
+
+let colorPara = document.querySelectorAll("p")
+colorPara.forEach(element => element.addEventListener("mouseenter", event => event.target.style.color = "slateGray")
+)
+
+let navParent = document.querySelector("nav.nav")
+navParent.addEventListener("click", e => console.log("parent"))
